4877c10d41b40a1d598b53f45cee661c41f50b35
Sziloyoo <sziloyoo@gmail.com>
2025-01-28 19:01:46 +0100

    debug camera

--- src/Experience/Camera.js
+++ src/Experience/Camera.js
@@ -8,22 +8,19 @@ export default class Camera {
         this.sizes = this.experience.sizes
         this.scene = this.experience.scene
         this.canvas = this.experience.canvas
+        this.debug = this.experience.debug
         this.distance = 24
         this.controls = null
 
         this.setInstance()
         this.setControls()
 
-        // Mouse move, camera effect
-        this.mousePosX = 0
-        this.mousePosY = 0
-        this.maxRotation = THREE.MathUtils.degToRad(1)
-        this.targetPosition = new THREE.Vector3()
-
-        window.addEventListener('mousemove', (event) => {
-            this.mousePosX = (event.clientX / window.innerWidth) - 0.5
-            this.mousePosY = (event.clientY / window.innerWidth) - 0.5
-        })
+        if (this.debug.active) {
+            this.debugFolder = this.debug.ui.addFolder({
+                title: 'Camera',
+            })
+            this.debugFolder.addBinding(this.controls, 'enabled', { label: 'Debug Camera' })
+        }
     }
 
     setInstance() {
@@ -36,22 +33,12 @@ export default class Camera {
         this.controls = new OrbitControls(this.instance, this.canvas)
         this.controls.enableDamping = true
         this.controls.maxPolarAngle = Math.PI
+        this.controls.enabled = false
     }
 
     resize() {
         this.instance.aspect = this.sizes.width / this.sizes.height
         this.instance.updateProjectionMatrix()
-    }
-
-    recalculate() {
-        const angleX = this.mousePosX * this.maxRotation
-        const angleY = this.mousePosY * this.maxRotation
-
-        const x = this.distance * Math.sin(angleX) * Math.cos(angleY)
-        const y = this.distance * Math.sin(angleY)
-        const z = this.distance * Math.cos(angleX) * Math.cos(angleY)
-
-        this.targetPosition.set(x, y, z)
     }
 
     update() {
